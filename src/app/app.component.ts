40f9bf7fce5e28e35554488e0987dc69df793e3b
Jérôme Viveret <j.viveret@criteo.com>
2017-06-12 21:44:45 +0200

    Ex1-IncDec-Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   template: `
-  Hello {{title}}
+    Hello world {{ total$ | async }}
+    <button #inc>Increment</button>
+    <button #dec>Decrement</button>
   `
 })
-export class AppComponent {
-  title = 'World';
+export class AppComponent implements AfterViewInit {
+  total$: Observable<number>
+
+  @ViewChild('inc') inc: ElementRef;
+  @ViewChild('dec') dec: ElementRef;
+
+  ngAfterViewInit(): void {
+  }
 }
